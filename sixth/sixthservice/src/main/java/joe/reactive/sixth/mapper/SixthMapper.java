21eab9d9be1b855a4fed5ba1cd242adac545ec7a
Sergey Fedorkov <fedorkov@farzoom.com>
2025-01-31 15:58:01 +0300

    added rest endpoints

--- sixth/sixthservice/src/main/java/joe/reactive/sixth/mapper/SixthMapper.java
+++ sixth/sixthservice/src/main/java/joe/reactive/sixth/mapper/SixthMapper.java
@@ -3,9 +3,9 @@ package joe.reactive.sixth.mapper;
 import joe.reactive.sixth.SixthDto;
 import joe.reactive.sixth.entity.ReactiveFields;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface SixthMapper {
     ReactiveFields map(SixthDto fifthDto, String type);
 }
