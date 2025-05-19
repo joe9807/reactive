22e5fa99bab8d406a398f933cc6cfc8f97c675ce
joe9807@gmail.com <joe9807@gmail.com>
2025-05-19 17:44:23 +0300

    added secondgrpc module

---
+++ second/secondgrpc/src/main/java/joe/grpc/SecondGrpcApplication.java
@@ -0,0 +1,13 @@
+package joe.grpc;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondGrpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondGrpcApplication.class, args);
+	}
+}
---
+++ second/secondgrpc/src/main/java/joe/grpc/service/SecondServiceImpl.java
@@ -0,0 +1,24 @@
+package joe.grpc.service;
+
+import io.grpc.stub.StreamObserver;
+import joe.model.Api;
+import joe.model.SecondServiceGrpc;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@Slf4j
+@GrpcService
+public class SecondServiceImpl extends SecondServiceGrpc.SecondServiceImplBase {
+
+    @Override
+    public void process2(Api.SecondDto request, StreamObserver<Api.ThirdDto> responseObserver) {
+        log.info("request: {}", request);
+        responseObserver.onNext(Api.ThirdDto.newBuilder()
+                        .setField111(request.getField11())
+                        .setField222(request.getField22())
+                        .setField333(request.getField33())
+                        .setField444(request.getField44())
+                .build());
+        responseObserver.onCompleted();
+    }
+}
