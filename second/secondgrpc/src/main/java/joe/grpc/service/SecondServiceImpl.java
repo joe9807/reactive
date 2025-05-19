ac82ed5adcc451bcb82b7e16fffd1f8ce423d88b
joe9807@gmail.com <joe9807@gmail.com>
2025-05-19 17:45:01 +0300

    added secondgrpc module

--- second/secondgrpc/src/main/java/joe/grpc/service/SecondServiceImpl.java
+++ second/secondgrpc/src/main/java/joe/grpc/service/SecondServiceImpl.java
@@ -5,10 +5,13 @@ import joe.model.Api;
 import joe.model.SecondServiceGrpc;
 import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.server.service.GrpcService;
+import org.springframework.beans.factory.annotation.Value;
 
 @Slf4j
 @GrpcService
 public class SecondServiceImpl extends SecondServiceGrpc.SecondServiceImplBase {
+    @Value("${next.port}")
+    private Integer nextPort;
 
     @Override
     public void process2(Api.SecondDto request, StreamObserver<Api.ThirdDto> responseObserver) {
