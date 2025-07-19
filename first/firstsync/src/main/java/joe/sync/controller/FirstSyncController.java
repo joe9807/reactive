c53f45e0193a5132d43e81a60fc5a459f7ed8d8b
joe9807@gmail.com <joe9807@gmail.com>
2025-07-19 16:44:37 +0300

    added CompletableFuture endpoint

--- first/firstsync/src/main/java/joe/sync/controller/FirstSyncController.java
+++ first/firstsync/src/main/java/joe/sync/controller/FirstSyncController.java
@@ -7,13 +7,12 @@ import joe.sync.mapper.FirstMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 
 @Slf4j
 @RestController
@@ -29,5 +28,44 @@ public class FirstSyncController {
     public JsonNode process(@RequestBody FirstDto firstDto){
         log.debug("SYNC: dto: {}", firstDto);
         return restTemplate.postForObject(URI.create(appConfig.getNext().getUrl()), mapper.map(firstDto), JsonNode.class);
+    }
+
+    @GetMapping("futures")
+    public CompletionStage<String> futures(){
+        return CompletableFuture.supplyAsync(()-> {
+            log.info(Thread.currentThread().getName()+" - supplyAsync");
+            return "supplyAsync";
+        }).thenCombineAsync(CompletableFuture.runAsync(() -> {
+            log.info(Thread.currentThread().getName()+" - thenCombineAsync(runAsync)");
+        }), (t, u)->{
+            log.info(Thread.currentThread().getName()+" - thenCombineAsync");
+            return t+(u == null?" runAsync":u);
+        })
+        .thenApplyAsync(v->{
+            log.info(Thread.currentThread().getName()+" - thenApplyAsync0");
+            return v+" thenApplyAsync0";
+        }).thenApplyAsync(v->{
+            log.info(Thread.currentThread().getName()+" - thenApplyAsync1");
+            return v+" thenApplyAsync1";
+        }).thenApplyAsync(v->{
+            log.info(Thread.currentThread().getName()+" - thenApplyAsync2");
+            return v+" thenApplyAsync2";
+        }).thenComposeAsync(v->{
+            log.info(Thread.currentThread().getName()+" - thenComposeAsync");
+            return CompletableFuture.supplyAsync(() -> {
+                log.info(Thread.currentThread().getName()+" - thenComposeAsync(supplyAsync)");
+                return v+" thenComposeAsync(supplyAsync)";
+            });
+        }).thenCombineAsync(
+            CompletableFuture.supplyAsync(() -> {
+                log.info(Thread.currentThread().getName()+" - thenCombineAsync(supplyAsync)");
+                return " thenCombineAsync(supplyAsync)";
+            }), (t, u)->{
+                log.info(Thread.currentThread().getName()+" - thenCombineAsync0");
+                return t+u;
+        }).thenApplyAsync(result->{
+            log.info("--------------------------------------------------- thenApplyAsync");
+            return result;
+        });
     }
 }
